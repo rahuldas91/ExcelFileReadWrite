97525d7511f5a07655edb7cce0fb4e5006185aef
RAHUL DAS <RAHUL DAS@LAPTOP-3GGOEH9Q>
2020-02-26 22:59:16 +0530

    initial commit

---
+++ excel-readWrite/src/main/java/com/seleniumwizard/app/Application.java
@@ -0,0 +1,37 @@
+package com.seleniumwizard.app;
+
+import java.io.File;
+
+import com.codoid.products.fillo.Connection;
+import com.codoid.products.fillo.Fillo;
+import com.codoid.products.fillo.Recordset;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		try {
+			
+			Fillo fillo=new Fillo();
+			String fileName = "Students.xlsx";
+			ClassLoader classLoader = new Application().getClass().getClassLoader();
+			 
+	        File file = new File(classLoader.getResource(fileName).getFile());
+			
+			Connection connection=fillo.getConnection(file.getAbsolutePath());
+			String strQuery="Select * from Students where Height=189 and First Name='David'";
+			Recordset recordset=connection.executeQuery(strQuery);
+
+			while(recordset.next()){
+			System.out.println(recordset.getField("Age"));
+			}
+
+			recordset.close();
+			connection.close();
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage() +" ----> "+e.getCause());
+		}
+
+	}
+}
